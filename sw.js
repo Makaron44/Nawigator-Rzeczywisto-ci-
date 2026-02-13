c2728b766bbad1bc0f17da2a726b63209bda9be9
Makaron44 <99068890+Makaron44@users.noreply.github.com>
2026-02-13 13:40:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'nr-transurfing-v2';
+const CACHE_NAME = 'nr-transurfing-v3'; // Zmieniłem wersję na v3, żeby wymusić odświeżenie
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
 ];
 
@@ -12,6 +14,17 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            )
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
@@ -21,4 +34,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
